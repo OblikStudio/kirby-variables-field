e7b4b80552323921546409d53d14e985b1d78e7b
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-29 13:46:01 +0300

    Tabs to spaces

--- index.php
+++ index.php
@@ -4,62 +4,69 @@ use Oblik\Variables\Manager;
 use Oblik\Variables\Util;
 
 Kirby::plugin('oblik/variables-field', [
-	'fields' => [
-		'variables' => [
-			'props' => [
-				'value' => function ($data = null) {
-					$lang = kirby()->language()->code();
+    'fields' => [
+        'variables' => [
+            'props' => [
+                'value' => function ($data = null) {
+                    $lang = kirby()->language()->code();
 
-					if (!$data) {
-						$handler = Manager::getHandler($lang);
-						$data = $handler->data;
+                    if (!$data) {
+                        $handler = Manager::getHandler($lang);
+                        $data = $handler->data;
 
-						if (!empty($data)) {
-							if ($this->variable) {
-								$data = $handler->find($this->variable);
-							}
-						} else {
-							if ($this->variable) {
-								$data = '';
-							} else {
-								$data = [];
-							}
-						}
-					}
+                        if (!empty($data)) {
+                            if ($this->variable) {
+                                $data = $handler->find($this->variable);
+                            }
+                        } else {
+                            if ($this->variable) {
+                                $data = '';
+                            } else {
+                                $data = [];
+                            }
+                        }
+                    }
 
-					return $data;
-				},
-				'variable' => function ($value = null) {
-					return $value;
-				},
-				'editor' => function ($value = []) {
-					return array_replace([
-						'keys' => false,
-						'values' => true,
-						'mutate' => true,
-						'sort' => true
-					], $value);
-				}
-			],
-			'save' => function ($value) {
-				$lang = kirby()->language()->code();
-				$handler = Manager::getHandler($lang);
+                    return $data;
+                },
+                'variable' => function ($value = null) {
+                    return $value;
+                },
+                'editor' => function ($value = []) {
+                    return array_replace([
+                        'keys' => false,
+                        'values' => true,
+                        'mutate' => true,
+                        'sort' => true
+                    ], $value);
+                }
+            ],
+            'save' => function ($value) {
+                $lang = kirby()->language()->code();
+                $handler = Manager::getHandler($lang);
 
-				if ($this->variable) {
-					Util::replace($this->variable, $value, $handler->data, true);
-				} else {
-					$handler->data = $value;
-				}
+                if ($this->variable) {
+                    Util::replace($this->variable, $value, $handler->data, true);
+                } else {
+                    $handler->data = $value;
+                }
 
-				$handler->write();
-				return null;
-			}
-		]
-	],
-	'tags' => [
-		'var' => require __DIR__ . DS . 'src' . DS . 'tag.php'
-	],
-	'translations' => [
-		'en' => require __DIR__ . DS . 'localization' . DS . 'en.php'
-	]
+                $handler->write();
+                return null;
+            }
+        ]
+    ],
+    'tags' => [
+        'var' => [
+            'attr' => [
+                'default'
+            ],
+            'html' => function ($tag) {
+                return t($tag->value) ?? $tag->default ?? '';
+            }
+        ]
+    ],
+    'translations' => [
+        'en' => include 'localization/en.php'
+    ]
 ]);
